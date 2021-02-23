8494d7115b3cfc65bd2a5acdae60b77bf730233e
ProjetInfo3B <73440556+ProjetInfo3B@users.noreply.github.com>
2021-02-23 22:36:26 +0100

    Correction
    Partie 1 100%
    - Correction de plusieurs problèmes et bugs
    Partie 2 30%
    - Fonction push() et pop() terminé, normalement
    
    Faire connexe et génération de labyrinthe intéréssant

--- Code/Partie_1.c
+++ Code/Partie_1.c
@@ -1,29 +1,50 @@
+/*
+Code de Salaï loïc
+
+Pour la compilation
+  gcc SALAI_Loïc_Partie1.c -g -Wall -o SALAI_Loïc_Partie1
+
+Pour l'éxécution :
+  ./SALAI_Loïc_Partie1 
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    On définit les caractères qui vont permettre de visualiser le labyrinthe :
+    '-' pour représenter les cases vides du labyrinthe 
+    'X' pour la représentation des murs du labyrinthe
+    '+' pour qu'on affiche les bords du labyrinthe 
+*/
+char AFF_VIDE = '-'; 
+char AFF_MUR = 'X'; 
+char AFF_BORD ='+';  
 
-int NB_COLONNES = 4;//Longueur
-int NB_LIGNES = 3; //largeur
-
+int NB_COLONNES = 2; // Correspond aux nombres de colonnes du labyrinthe
+int NB_LIGNES = 3; // Correspond aux nombres de lignes du labyrinthe
 char* Grille=NULL;
 
-// Pour l'affichage du labyrtinthe en ligne de commande 
-#define AFF_VIDE '-' //Caractère représentant les cases vides pour l'affichage
-#define AFF_MUR 'X' //Caractère représentant les murs pour l'affichage
-#define AFF_BORD '+' //Caractère représentant les bords pour l'affichage 
+/* 
+    La fonction getID retourne un entier qui est l'indice de la case dans à la ligne et la colonne indiqué en paramètres
+    Paramètres : le numéro de ligne et le numéro de colonne
+*/
 
-
-//retourne l'identifiant d'une case dont on donne les coordonnées
 int getID(int ligne, int colonne)
 {  
-   return 4*ligne+colonne;
+   return NB_COLONNES*ligne+colonne;
 }
 
-//retourne la première coordonée (ligne) d'une case dont on donne l'identifiant
+/* 
+    La fonction getLigne retourne un entier qui est la ligne de la case à l'indice id
+    Paramètre : l'indice de la case
+*/
+
 int getLigne(int id)
 {   
-    int col = 0; //pour récupérer la coordonnée correspondant à la colonne
-    int lgn = 0; //pour récupérer la coordonnée correspondant à la ligne
+    int col = 0; // Variable qui contiendra la coordonnée correspondant à la colonne
+    int lgn = 0; // Variable qui contiendra la coordonnée correspondant à la ligne et qui est retournée en fin de fonction
     for (int i = 0; i < id; i++)
     {
         if (col != NB_LIGNES) col = col + 1;
@@ -36,10 +57,14 @@ int getLigne(int id)
     return lgn ;
 }
 
-//retourne la première coordonée (ligne) d'une case dont on donne l'identifiant
+/* 
+    La fonction getColonne retourne un entier qui est la colonne de la case à l'indice id
+    Paramètre : l'indice de la case
+*/
+
 int getColonne(int id)
 {   
-    int col = 0; //pour récupérer la coordonnée correspondant à la colonne
+    int col = 0; // Variable qui contiendra la coordonnée correspondant à la colonne et qui sera retourné 
     for (int i = 0; i < id; i++)
     {
         if (col != NB_COLONNES) col = col + 1;
@@ -48,35 +73,44 @@ int getColonne(int id)
     return col ;
 }
 
-//place la valeur x dans le case de coordonnée (i,j)
+/*
+    La fonction modifie permet de donner une valeur à la case, dont la ligne et la colonne sont données en paramètre, sous la forme d'un caractère 
+    Les caractères possibles sont : 
+    - 0 lorsque c'est vide
+    - 1 lorsqu'il y a un mur
+    (- 2 pour marqué une case de la grille ) Voir fonction connexe
+    Paramètres : la ligne de la case, la colonne de la case, et le caractère x que l'on veut mettre dans la case
+*/
+
 void modifie(int ligne, int colonne, char x)
 {
     Grille[getID(ligne, colonne)] = x ;
 }
 
-//retourne la valeur de la case de coordonnées (i,j)
+/* 
+    La fonction lit permet de retourner le caractère (char) contenu d'une case
+    Paramètres : la ligne et la colonne de la case dont on souhaite retourner le caractère (char)
+*/
 char lit(int ligne, int colonne)
 {
     return Grille[getID(ligne,colonne)];
 }
 
-//affiche la grille
+/* 
+    La fonction affiche nous permet de visualiser la grille 
+*/
 void affiche()
 {   for (int i = -1; i <= NB_LIGNES; i++)
     {
         for (int j = -1; j <= NB_COLONNES; j++)
-        {      
+        {    
             if (i == -1 || j == -1 || i == NB_LIGNES || j ==  NB_COLONNES)
             {
                 printf("%c", AFF_BORD);
             }
-            else if (lit(i,j) == 0)
-                {
-                    printf("%c", AFF_VIDE);
-                }
-            else if (lit(i,j) == 1)
+            else
             {
-                printf("%c", AFF_MUR);
+                printf("%c", lit(i,j));
             }
         }
         printf("\n");
@@ -87,8 +121,14 @@ void affiche()
 int main()
 {   
     Grille = (char*)calloc(NB_LIGNES*NB_COLONNES,sizeof(char));
-    modifie(1,1,1);
-    modifie(2,3,1);
+    for (int i = 0; i < NB_LIGNES; i++)
+    {
+        for (int j = 0; j < NB_COLONNES; j++)
+        {
+            modifie(i,j, AFF_VIDE);
+        }
+    }
+    modifie(1,1,AFF_MUR);
     affiche();
     free(Grille);
     return 0;
